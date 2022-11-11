83b5ce0ce21f8e6bc4479745243a521d1400470c
KaraJH <karas.jones@yahoo.com>
2022-11-11 11:41:44 -0700

    Tests shifter methods with passing tests

--- lib/shifter.rb
+++ lib/shifter.rb
@@ -3,28 +3,28 @@ class Shifter
   def key_generator
     (0..9).to_a.sample(5).join('')
   end
-  #
-  # def key_pair_generator(key = key_generator)
-  #   paired_keys = []
-  #   key_generator.each_cons(2) do |key|
-  #     paired_keys << key
-  #   end
-  #   paired_keys
-  # end
-  #
-  # def flatten_key_pairs(key = key_generator)
-  #   key_pair_generator.flat_map do |key_pair|
-  #     key_pair.join.to_i
-  #   end
-  # end
-  #
-  # def set_date
-  #   Date.today.strftime("%m%d%y")
-  # end
-  #
-  # def square_date(date = set_date)
-  #   squared_date = set_date.to_i ** 2
-  # end
+  
+  def key_pair_generator(key = key_generator)
+    paired_keys = []
+    key_generator.each_cons(2) do |key|
+      paired_keys << key
+    end
+    paired_keys
+  end
+
+  def flatten_key_pairs(key = key_generator)
+    key_pair_generator.flat_map do |key_pair|
+      key_pair.join.to_i
+    end
+  end
+
+  def set_date
+    Date.today.strftime("%m%d%y")
+  end
+
+  def square_date(date = set_date)
+    squared_date = set_date.to_i ** 2
+  end
   #
   # def last_four_date(date = set_date)
   #   last_four_digits = []

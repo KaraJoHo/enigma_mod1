c9ee52bc52904fd4012fa95269818b76278d1245
KaraJH <karas.jones@yahoo.com>
2022-11-11 09:38:41 -0700

    Creates character set test

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -7,4 +7,13 @@ RSpec.describe Enigma do
 
     expect(enigma).to be_a(Enigma)
   end
+
+  describe '#character_set' do
+    it 'has an array of the alphabet' do
+      enigma = Enigma.new
+
+      expected_set = ("a".."z").to_a << " "
+      expect(enigma.character_set).to eq(expected_set)
+    end
+  end
 end

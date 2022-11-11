075abb377e70aa5c63f5a4ec408d53984ba561f7
KaraJH <karas.jones@yahoo.com>
2022-11-11 09:43:16 -0700

    Tests key generator method

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -16,4 +16,12 @@ RSpec.describe Enigma do
       expect(enigma.character_set).to eq(expected_set)
     end
   end
+
+  describe '#key_generator' do
+    it ' generates a random set of 5 numbers' do
+      enigma = Enigma.new
+
+      expect(enigma.key_generator.length).to eq(5)
+    end
+  end
 end

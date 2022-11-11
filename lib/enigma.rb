47a28b5887d52fd79fe9a3519682433039d73c78
KaraJH <karas.jones@yahoo.com>
2022-11-11 10:21:52 -0700

    Creates flatten key pair method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -8,11 +8,17 @@ class Enigma
     (0..9).to_a.sample(5).join('')
   end
 
-  def key_pair_generator
+  def key_pair_generator(key = key_generator)
     paired_keys = []
     key_generator.each_cons(2) do |key|
       paired_keys << key
     end
     paired_keys
+  end
+
+  def flatten_key_pairs(key = key_generator)
+    key_pair_generator.flat_map do |key_pair|
+      key_pair.join.to_i
+    end
   end
 end

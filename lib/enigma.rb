24a2378fda4ca89c3b14a754540e195e15adaa99
KaraJH <karas.jones@yahoo.com>
2022-11-11 11:16:21 -0700

    Adjusts offset method with passing test

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -38,6 +38,17 @@ class Enigma
   end
 
   def offset(date = set_date)
-    last_four_date
+    last_four_date.digits.reverse
+  end
+
+  def shifter_set(keys, offsets)
+    key_nums = keys
+    offset_nums = offsets
+    final_shifter_set = []
+
+    keys.each_with_index do |key, index|
+      final_shifter_set << key_nums[index] + offset_nums[index].to_i
+    end
+    final_shifter_set
   end
 end

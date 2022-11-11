62dda30a672316bbcb9ff550b54803c290ac9ee1
KaraJH <karas.jones@yahoo.com>
2022-11-11 10:08:45 -0700

    Creates key [air generator method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -7,4 +7,12 @@ class Enigma
   def key_generator
     (0..9).to_a.sample(5).join('')
   end
+
+  def key_pair_generator
+    paired_keys = []
+    key_generator.each_cons(2) do |key|
+      paired_keys << key
+    end
+    paired_keys
+  end
 end

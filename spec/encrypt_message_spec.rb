a3ac08d10697dde4bddf7b63ede2c6e182035b4e
KaraJH <karas.jones@yahoo.com>
2022-11-12 19:04:59 -0700

    Creates test for child class of cryptor

--- spec/encrypt_message_spec.rb
+++ spec/encrypt_message_spec.rb
@@ -1,10 +1,12 @@
-require 'rspec'
-require './lib/encrypt_message'
 require './lib/shifter'
+require './lib/cryptor'
+require './lib/encrypt_message'
 
 RSpec.describe EncryptMessage do
-  it 'exists' do
+  it 'exists and is a child of cryptor' do
     encrypt_message = EncryptMessage.new
+
     expect(encrypt_message).to be_a(EncryptMessage)
+    expect(EncryptMessage < Cryptor).to eq(true)
   end
 end

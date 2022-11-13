2b30beaa43886b015b9cbdf9ee39111c4f712a2f
KaraJH <karas.jones@yahoo.com>
2022-11-12 17:48:48 -0700

    Fixes indentation

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -13,48 +13,48 @@ class Enigma
 
   def cipher(message, shifter)
     translated_message = ''
+    message = message.downcase
 
     message.each_char.with_index do |character, i|
-      lower_char = character.downcase
-
-      if character_set.include?(lower_char)
-        char_idx = character_set.index(lower_char)
+      if character_set.include?(character)
+        char_idx = character_set.index(character)
         shift = shifter[i % shifter.length]
         new_index = (char_idx + shift) % character_set.length
 
-        if (char_idx + shift) < 27
+        if (char_idx + shift) < character_set.length
           translated_message << character_set[new_index]
-          else
-            translated_message << character_set[(new_index) % character_set.length]
-          end
         else
+          translated_message << character_set[(new_index) % character_set.length]
+        end
+      else
         translated_message << character
       end
     end
-  translated_message
+      translated_message
   end
+
 
   def decryptor(message, shifter)
     translated_message = ''
+    message = message.downcase
 
     message.each_char.with_index do |character, i|
-      lower_char = character.downcase
       reverse_character_set = character_set.reverse
 
-      if character_set.include?(lower_char)
-        char_idx = reverse_character_set.index(lower_char)
+      if character_set.include?(character)
+        char_idx = reverse_character_set.index(character)
         shift = shifter[i % shifter.length]
         new_index = (char_idx + shift) % reverse_character_set.length
 
-        if (char_idx + shift) < 27
+        if (char_idx + shift) < reverse_character_set.length
           translated_message << reverse_character_set[new_index]
-          else
-            translated_message << reverse_character_set[(new_index) % reverse_character_set.length]
-          end
         else
+          translated_message << reverse_character_set[(new_index) % reverse_character_set.length]
+        end
+      else
         translated_message << character
       end
     end
-  translated_message
+      translated_message
   end
 end

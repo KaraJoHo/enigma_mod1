e480d313f3464adad88e1f50bd4149a5ed6d0789
KaraJH <karas.jones@yahoo.com>
2022-11-12 19:07:23 -0700

    Tests that parent cipher method works in encrypt message child class

--- spec/encrypt_message_spec.rb
+++ spec/encrypt_message_spec.rb
@@ -9,4 +9,26 @@ RSpec.describe EncryptMessage do
     expect(encrypt_message).to be_a(EncryptMessage)
     expect(EncryptMessage < Cryptor).to eq(true)
   end
+
+  describe '#cipher' do
+    it ' take a message and iterates over the character_set to return encrypted string' do
+      cryptor = Cryptor.new
+      encrypt_message = EncryptMessage.new
+      shifter = Shifter.new
+      expected_flattened = [2,27,71,15]
+      expected_offset = [1,0,2,5]
+
+      allow(encrypt_message.shifter).to receive(:offset).and_return([02,27,71,15])
+      allow(encrypt_message.shifter).to receive(:flatten_key_pairs).and_return([01,00,02,05])
+      allow(encrypt_message.shifter).to receive(:shifter_set).and_return([3,27,73,20])
+
+      message = "hello world"
+      message2 = "hello World!"
+      message3 = "HeLlO wOrlD!!"
+
+      expect(encrypt_message.cipher(message, encrypt_message.shifter.shifter_set(expected_flattened, expected_offset))).to eq("keder ohulw")
+      expect(encrypt_message.cipher(message2, encrypt_message.shifter.shifter_set(expected_flattened, expected_offset))).to eq("keder ohulw!")
+      expect(encrypt_message.cipher(message3, encrypt_message.shifter.shifter_set(expected_flattened, expected_offset))).to eq("keder ohulw!!")
+    end
+  end
 end

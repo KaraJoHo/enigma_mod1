b5e7e729c77284528a1966a999121f83df26f2b1
KaraJH <karas.jones@yahoo.com>
2022-11-12 18:54:40 -0700

    Adjusts enigma test for the new cryptor class

--- spec/cryptor_spec.rb
+++ spec/cryptor_spec.rb
@@ -16,6 +16,40 @@ RSpec.describe Cryptor do
     expect(cryptor.shifter).to be_a(Shifter)
   end
 
+  describe '#character_set' do
+    it 'has an array of the alphabet' do
+      cryptor = Cryptor.new
+
+      expected_set = ("a".."z").to_a << " "
+      expect(cryptor.character_set).to eq(expected_set)
+    end
+  end
+
+  it 'has a date from shifter' do
+    cryptor = Cryptor.new
+    shifter = Shifter.new
+
+    allow(cryptor.shifter).to receive(:set_date).and_return("111122")
+    expect(cryptor.shifter.set_date).to eq("111122")
+  end
+
+  it 'has offsets from shifter' do
+    cryptor = Cryptor.new
+    shifter = Shifter.new
+
+    allow(cryptor.shifter).to receive(:offset).and_return([8,8,8,4])
+    expect(cryptor.shifter.offset).to eq([8,8,8,4])
+  end
+
+  it 'has a shifter set' do
+    cryptor = Cryptor.new
+    shifter = Shifter.new
+
+    allow(cryptor.shifter).to receive(:offset).and_return([8,8,8,4])
+    allow(cryptor.shifter).to receive(:flatten_key_pairs).and_return([12, 23, 34, 45])
+    expect(cryptor.shifter.shifter_set(cryptor.shifter.flatten_key_pairs, cryptor.shifter.offset)).to eq([20, 31, 42, 49])
+  end
+
   describe '#cipher' do
     it ' take a message and iterates over the character_set to return encrypted string' do
       cryptor = Cryptor.new

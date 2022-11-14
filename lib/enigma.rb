f83c658cd2b020451cadd6a2a7ff5229e232af8d
KaraJH <karas.jones@yahoo.com>
2022-11-14 13:58:06 -0700

    Creates decrypt method with passing test

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -11,35 +11,12 @@ class Enigma
   end
 
   def encrypt(message, key = nil, date = nil)
-    #require 'pry' ;binding.pry
     @encrypt_message = EncryptMessage.new(message, key, date)
     @encrypt_message.encryption_result_set
   end
 
-
-  
-
-  def decryptor(message, shifter)
-    translated_message = ''
-    message = message.downcase
-
-    message.each_char.with_index do |character, i|
-      reverse_character_set = character_set.reverse
-
-      if character_set.include?(character)
-        char_idx = reverse_character_set.index(character)
-        shift = shifter[i % shifter.length]
-        new_index = (char_idx + shift) % reverse_character_set.length
-
-        if new_index < reverse_character_set.length
-          translated_message << reverse_character_set[new_index]
-        else
-          translated_message << reverse_character_set[(new_index) % reverse_character_set.length]
-        end
-      else
-        translated_message << character
-      end
-    end
-      translated_message
+  def decrypt(message, key = nil, date = nil)
+    @decrypt_message = DecryptMessage.new(message, key, date)
+    @decrypt_message.decryption_result_set
   end
 end
